87e2be2750dbf011160a552ee5683984ad07c23a
Maxime Touroute <maxime.touroute@gmail.com>
2015-03-08 20:30:06 +0100

    Création du découpage en grille

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,7 @@
-
-#include "Panorama.cpp"
-
-
-
-int thresh = 1;
-int max_thresh = 10;
+#include "Panorama.h"
 
 Mat img = imread("img/image_1.jpg", CV_LOAD_IMAGE_COLOR);
-Mat img2 = imread ("img/imag_2.jpg", CV_LOAD_IMAGE_COLOR);
-
-void thresh_callback(int, void*);
-void save(int state, void* userdata);
+Mat img2 = imread ("img/image_2.jpg", CV_LOAD_IMAGE_COLOR);
 
 int main(int argc, char *argv[])
 {
@@ -18,37 +9,13 @@ int main(int argc, char *argv[])
 	namedWindow("Projet Panorama", CV_WINDOW_AUTOSIZE);
 
 	// Création de deux fenêtres avec les deux images à fusionner
-	imshow("Image 1", img);
-	imshow("image 2", img);
+	imshow("image 1", img);
+	imshow("image 2", img2);
 
-
+    panorama_process(img, img2);
 	//createTrackbar(" Contour level:", "lena", &thresh, max_thresh, thresh_callback);
 	//thresh_callback(0, 0);
-
+    printf("Hola\n");
 	waitKey(0);
 	return 0;
-}
-
-void thresh_callback(int, void*)
-{
-	for (int i = 0; i<img.rows; i++)
-	{
-		img2.at<Vec3b>(i, 0) = Vec3b(0, 0, 0);
-	}
-
-	for (int i = 0; i<img.cols; i++)
-	{
-		img2.at<Vec3b>(0, i) = Vec3b(0, 0, 0);
-	}
-
-	for (int i = 1; i<img.rows; i++)
-	{
-		for (int j = 1; j<img.cols; j++)
-		{
-			img2.at<Vec3b>(i, j) = thresh*((img.at<Vec3b>(i, j) - img.at<Vec3b>(i - 1, j)) + (img.at<Vec3b>(i, j) - img.at<Vec3b>(i, j - 1)));
-		}
-	}
-
-	namedWindow("lena2", CV_WINDOW_AUTOSIZE);
-	imshow("lena2", img2);
 }
---
+++ src/Panorama.h
@@ -0,0 +1,132 @@
+
+#include "Canvas.h"
+#define GRID_SIZE 128
+
+using namespace cv;
+using namespace std;
+
+
+/*
+* image_to_grid : convertit une image en grille de petites images
+* return: tableau de petites images
+*/
+void image_to_grid(Mat img, Mat mat_array[], int grid_size)
+{
+    // Pour l'instant, ne gère que le cas parfait où la grille peut se former sans problèmes !
+    //Mat* mat_array= (Mat*) malloc(sizeof(Mat) * (img.rows*img.cols/grid_size));
+   // Mat mat_array[img.rows*img.cols/GRID_SIZE];
+    if(img.rows % grid_size != 0)
+    {
+        printf("Warning : image_to_grid() taille de grille pas compatible avec l'image :\n---> grid:%i pixelslignes:%i\n", grid_size, img.rows);
+    }
+    else if(img.cols % grid_size != 0)
+    {
+        printf("Warning : image_to_grid() taille de grille pas compatible avec l'image :\n---> grid:%i pixelscols:%i\n", grid_size, img.cols);
+    }
+    else // Cas parfait
+    {
+
+        int index = 0;
+
+        for(int i = 0 ; i < img.rows ; i+=grid_size )
+        {
+            //Boucle sur les colonnes
+            for(int j = 0 ; j < img.cols ; j+=grid_size, index++)
+            {
+
+                // La zone de crop.
+                            // x           y           width       height
+                Rect roi (j, i, grid_size, grid_size);
+                printf("%i,%i\n", j, i);
+                // Crop de la zone voulue.
+                mat_array[index] = img(roi).clone();
+
+            }
+        }
+       // mat_return = mat_array.clone();
+
+    }
+        // Boucle sur les lignes
+
+}
+
+
+/*
+Retourne le panorama
+Paramètres : les deux images à fusionner
+*/
+void panorama_process(Mat img1, Mat img2)
+{
+    // L'image finale
+    Mat panorama_image;
+
+    // Etape 0: Faire un process pour se débarasser des informations que l'on veut pas pour la fusion panorama.
+    // noir et blanc ?
+    // Image de detection de contours ?
+
+    Mat gray1(img1.rows, img1.cols, CV_8UC1); // CV8UC1 means 8 bits, 1channel.
+    Mat gray2(img2.rows, img2.cols, CV_8UC1);
+
+    cvtColor(img1, gray1, CV_RGB2GRAY );
+
+    cvtColor(img2, gray2, CV_RGB2GRAY );
+
+    // Etape 1: On découpe la première image en petits éléments carrés
+    //imshow("Gray 1", gray1);
+    //imshow("Gray 2", gray2);
+    // On place les données
+    //Mat* part_img1 = image_to_grid(gray1, 10);
+    printf("Hola\n");
+
+    Mat part_img1[gray1.rows*gray1.cols/GRID_SIZE];
+
+    image_to_grid(gray1, part_img1, GRID_SIZE); // grid_size doit etre compatible avec l'image !
+
+    // Utilisation d'un vecteur pour stocker les éléments
+    vector<Mat> vect_part_img1;
+
+    for(int i = 0 ; i < 16 ; i++)
+    {
+        vect_part_img1.push_back(part_img1[i]);
+    }
+
+        imshow("grid", makeCanvas(vect_part_img1, 512, 4));
+        imshow("img2 gray", gray2);
+
+
+    // Etape 2: A l'aide d'un algorithme, on attribue des poids à chaque case
+
+
+    // Plus le poids est élevé, plus la portion d'image a des zones d'intérêt faciles à identifier
+  // int poids_img1[100];
+
+    // Etape 3: On prend la case de poids le + élevé de l'image 1, et on la teste avec l'image 2
+    /* Méthode précise à définir.
+
+    1. Utiliser avec le morceau d'img1 le + probable de coller :
+        Par défaut: poids élevé, et sur le coté droit de l'image (à faire évoluer plus tard)
+        (celà dépend du sens de fusion (si le morceau est opposé au sens de fusion, ça colle pas.
+        Par défaut
+
+    2. Tester la similarité de cet élément avec l'autre image. On s'embête pas et on utilise un algo d'OpenCV pour ça:
+        (on fera nous même plus tard si on a le temps)
+
+        http://docs.opencv.org/doc/tutorials/features2d/feature_flann_matcher/feature_flann_matcher.html
+        http://stackoverflow.com/questions/15572357/compare-the-similarity-of-two-images-with-opencv
+
+    3. Retourner des valeurs x et y de décalage pour pouvoir coller l'image 2 à l'image 1 (on oublie la rotation pour l'instant)
+
+    4. Afficher les deux images, avec un carré autour des zones repérées
+
+    5. Si la zone correspond pas, permettre à l'utilisateur de cliquer sur un bouton reessayer
+    Et ça sera déjà bien.
+
+    */
+    printf("Hola sortie\n");
+}
+
+
+
+
+
+
